08488cedd2e63258708a69b497d7211c7b47edaa
Rafael Amaral <rafael.amaral@monocard.com.br>
2024-02-01 15:40:05 -0300

    feat: Per column in application

--- apps/frontend/src/components/table/columns.tsx
+++ apps/frontend/src/components/table/columns.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@chakra-ui/react";
+import { Avatar, Stack, Tooltip } from "@chakra-ui/react";
 import { ISchedule } from "@pet-shop/entities/schedule";
 import { StatusEnum } from "@pet-shop/entities/statusenum";
 import { TableColumn } from "react-data-table-component";
@@ -9,18 +9,36 @@ export function columns(
 ) {
   const columns: TableColumn<ISchedule>[] = [
     {
+      name: "Pet",
+      cell: (row) => (
+        <Stack>
+          <Tooltip label={row?.client?.pet?.name} placement={"top"} hasArrow>
+            <Avatar
+              name={row?.client?.pet?.name}
+              src={row?.client?.pet?.imageUrl}
+              w="2rem"
+              h="2rem"
+            />
+          </Tooltip>
+        </Stack>
+      ),
+      maxWidth: "120px",
+    },
+    {
       name: "Cliente",
       selector: (row) => row?.client?.name || "Sem nome",
     },
     {
       name: "Data",
       selector: (row) => {
+        console.log(row);
+        const addZero = (n: number) => (n < 10 ? `0${n}` : n);
         const date = new Date(Number(row.timestamp));
-        const day = date.getDate();
-        const month = date.getMonth() + 1;
+        const day = addZero(date.getDate());
+        const month = addZero(date.getMonth() + 1);
         const year = date.getFullYear();
-        const hour = date.getHours();
-        const minute = date.getMinutes();
+        const hour = addZero(date.getHours());
+        const minute = addZero(date.getMinutes());
         return `${day}/${month}/${year} ${hour}:${minute}`;
       },
     },
